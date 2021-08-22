03817eac7ecbeeea800a230e364fd7112234b9b1
Jenrry <jenrrymonegro58@gmail.com>
2021-08-22 19:28:05 -0400

    Subiendo al repositorio

---
+++ Practica 1 CursoASP/Pages/Index.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Practica_1_CursoASP.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
